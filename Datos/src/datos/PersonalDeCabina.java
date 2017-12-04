206a0fd245eedc2a16b580a40c27c22dfaad7d4b
Adrian Verdugo <adrianverdugo273@gmail.com>
2017-12-04 13:27:12 -0300

    update in javaOne

--- Datos/src/datos/PersonalDeCabina.java
+++ Datos/src/datos/PersonalDeCabina.java
@@ -7,12 +7,34 @@ package datos;
 public abstract class PersonalDeCabina extends Persona {
     
     private String nacionalidad;
-
+    
+    /**
+     * 
+     * @param nacionalidad del persona de la cabina
+     * @param rut del persona de la cabina
+     * @param nombre del persona de la cabina
+     * @param apellido del persona de la cabina
+     * @param edad del persona de la cabina
+     */
     public PersonalDeCabina(String nacionalidad, String rut, String nombre, String apellido, int edad) {
         super(rut, nombre, apellido, edad);
         this.nacionalidad = nacionalidad;
     }
+    
+    /**
+     * 
+     * @param rut del persona de la cabina
+     * @param nombre del persona de la cabina
+     * @param apellido del persona de la cabina
+     * @param edad del persona de la cabina
+     */
+    public PersonalDeCabina(String rut, String nombre, String apellido, int edad) {
+        super(rut, nombre, apellido, edad);
+    }
 
+    public PersonalDeCabina() {
+    }
+    
     /**
      * 
      * @return la nacionalidad del personal de cabina 
